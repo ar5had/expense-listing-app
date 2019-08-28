0ceb481871467bd25d41cf1b8c0a3055615b5386
Arshad Khan <arshdkhn1@gmail.com>
2019-08-28 19:55:28 +0530

    Add graphql server to replace REST api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,24 @@
-import { NextPage } from "next";
-import Link from "next/link";
-import fetch from "isomorphic-unfetch";
-import { baseUrl } from '../config';
+import { NextPage } from 'next'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+import { baseUrl } from '../config'
 
-const Home: NextPage<object> = data => (
+const Home: NextPage = () => (
   <>
     <h1>Expense List</h1>
     <Link href="/item">
       <a>/item</a>
     </Link>
   </>
-);
+)
 
-Home.getInitialProps = async ({ req }) => {
-  const res = await fetch(`${baseUrl}/expenses/5b995dff2e3cb74644948a66ff`);
-  const data = await res.json();
+// Home.getInitialProps = async ({ req }) => {
+//   const res = await fetch(`${baseUrl}/expenses/5b995dff2e3cb74644948a66ff`)
+//   const data = await res.json()
 
-  return {
-    data
-  };
-};
+//   return {
+//     data
+//   }
+// }
 
-export default Home;
+export default Home
---
+++ server/data/schema.ts
@@ -0,0 +1,46 @@
+import { gql } from 'apollo-server'
+
+export const typeDefs = gql`
+  type Amount {
+    value: String!
+    currency: String!
+  }
+
+  type User {
+    first: String!
+    last: String!
+    email: String!
+  }
+
+  type Receipt {
+    id: ID!
+    url: String!
+  }
+
+  type Expense {
+    id: ID!
+    amount: Amount!
+    date: String!
+    merchant: String!
+    receipts: [Receipt]!
+    comment: String
+    category: String
+    user: User!
+  }
+
+  type AllExpensesResponse {
+    expenses: [Expense]!
+    total: Int
+  }
+
+  type Query {
+    expenses(limit: Int, offset: Int): AllExpensesResponse!
+    expense(id: ID!): Expense!
+    receipts: [Receipt]!
+  }
+
+  type Mutation {
+    updateComment(id: ID!, comment: String): Expense!
+    updateReceipt(id: ID!, receiptUrls: [String!]!): Expense!
+  }
+`
