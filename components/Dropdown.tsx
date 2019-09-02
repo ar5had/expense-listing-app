c2285579b03b99caf02790c33c0d8c70e4a14872
Arshad Khan <arshdkhn1@gmail.com>
2019-09-02 18:47:31 +0530

    Add expense filter (#21)

---
+++ components/Dropdown.tsx
@@ -0,0 +1,33 @@
+import Select from 'react-select'
+import { ReactSelectStyles, ReactSelectTheme } from './styles/ReactSelectStyles'
+import { SelectComponentsProps } from 'react-select/src/Select'
+import styled from 'styled-components'
+import { gts } from '../lib/getThemeStyle'
+
+// NOTE: style props approach is preferred over targetting styles using css-classnames, that's why
+// all the styling is done using styles props except few styles that can't be set by styles props.
+// So in order to sync the react-select styles with our theme, we need to use className-prefix css
+const DropdownStyles = styled.div`
+  .react-select__single-value {
+    color: ${gts('darkGrey')};
+  }
+
+  .react-select__control--is-focused {
+    .react-select__single-value {
+      color: ${gts('black')} !important;
+    }
+  }
+`
+
+const Dropdown: React.FC<SelectComponentsProps> = (props) => (
+  <DropdownStyles>
+    <Select
+      styles={ReactSelectStyles}
+      classNamePrefix="react-select"
+      theme={ReactSelectTheme}
+      {...props}
+    />
+  </DropdownStyles>
+)
+
+export default Dropdown
---
+++ components/FilterExpense.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import { FilterExpenseProps } from '../types/components'
+import FilterType from './FilterType'
+import Input from './styles/Input'
+import { gts } from '../lib/getThemeStyle'
+
+const StyledFilter = styled.div`
+  margin-bottom: ${gts('lgMargin')}px;
+  position: relative;
+  input {
+    width: 100%;
+    padding: 10px 150px 10px 5px;
+    transition: 0.2s;
+    line-height: 1;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+  }
+`
+
+const FilterExpense: React.FC<FilterExpenseProps> = ({
+  filterText,
+  changeFilterText,
+  filterType,
+  changeFilterType
+}) => {
+  return (
+    <StyledFilter>
+      <Input
+        placeholder="Filter expense..."
+        value={filterText}
+        onChange={({ currentTarget: { value } }) => changeFilterText(value)}
+      />
+      <FilterType filterType={filterType} changeFilterType={changeFilterType} />
+    </StyledFilter>
+  )
+}
+
+export default FilterExpense
