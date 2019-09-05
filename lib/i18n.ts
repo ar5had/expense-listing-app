cb2a4c3c553399c1e186648ddcd59cc91a88c994
Arshad Khan <arshdkhn1@gmail.com>
2019-09-05 21:31:40 +0530

    Stop empty searches

---
+++ lib/i18n.ts
@@ -0,0 +1,9 @@
+import NextI18Next from 'next-i18next'
+
+const NextI18NextInstance = new NextI18Next({
+  defaultLanguage: 'en',
+  otherLanguages: ['fr']
+})
+
+export default NextI18NextInstance
+export const { appWithTranslation, withTranslation } = NextI18NextInstance
